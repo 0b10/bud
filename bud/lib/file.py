4264b3a9c6986a4e5a073e40766ec3268e3d7ac0
0b10 <0b10@pm.me>
2020-02-19 19:30:24 +0000

    build(license): add missing licenses

--- bud/lib/file.py
+++ bud/lib/file.py
@@ -19,14 +19,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-from typing import Optional
-
-
-class SingletonMeta(type):
-    _instance: Optional[type] = None
-
-    def __call__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
-        return cls._instance
